97ffd059e63125408341339486ea1f5887e8b9d9
Tudor Lechințan <lechintantudor02@gmail.com>
2024-04-06 10:29:17 +0300

    Add more abbreviations and clean up fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,16 +2,26 @@
 set -U fish_greeting
 
 # Environment
+set -gx EDITOR /bin/helix
 set -gx SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/ssh-agent.socket"
 
 # Bindings
-bind \ce helix
+bind \ce $EDITOR
 
 # Program abbreviations 
+abbr -a c cargo
 abbr -a cd.. cd ..
+abbr -a cr cargo run
+abbr -a crr cargo run --release
+abbr -a dfs dotfiles
+abbr -a dfsa dotfiles add .
+abbr -a dfss dotfiles status
+abbr -a g git
+abbr -a ga git add .
+abbr -a gs git status
 abbr -a hx helix
-abbr -a vi nvim
 abbr -a vim nvim
+abbr -a vi nvim
 
 # Program replacements
 type -q bat && alias cat bat
@@ -21,18 +31,16 @@ type -q rg && alias grep rg
 type -q zoxide && zoxide init --cmd cd fish | source
 
 # Zellij integration
-function zellij_update_name --on-variable PWD
-    if not set -q ZELLIJ
-        return
+if set -q ZELLIJ
+    function zellij_update_name --on-variable PWD
+        if test $PWD = $HOME
+            set -f directory '~'
+        else
+            set -f directory $(basename $PWD)
+        end
+
+        zellij action rename-pane $directory
     end
 
-    if test $PWD = $HOME
-        set -f directory '~'
-    else
-        set -f directory $(basename $PWD)
-    end
-
-    zellij action rename-pane $directory
+    zellij_update_name
 end
-
-zellij_update_name
