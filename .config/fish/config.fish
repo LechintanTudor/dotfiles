38dafa2db1d2e1a0ec900e8583ea75b904b4a25f
Tudor Lechințan <lechintantudor02@gmail.com>
2024-05-03 11:36:30 +0300

    Add new fish abbreviations

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,36 +1,10 @@
 # Disable startup message
 set -U fish_greeting
 
-# Environment
-set -gx EDITOR /bin/helix
-set -gx SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/ssh-agent.socket"
-
-# Bindings
-bind \ce $EDITOR
-
-# Program abbreviations 
-abbr -a c cargo
-abbr -a cd.. cd ..
-abbr -a cr cargo run
-abbr -a crr cargo run --release
-abbr -a dfs dotfiles
-abbr -a dfsa dotfiles add .
-abbr -a dfss dotfiles status
-abbr -a g git
-abbr -a ga git add .
-abbr -a gs git status
-abbr -a hx helix
-abbr -a vim nvim
-abbr -a vi nvim
-
-# Program replacements
-type -q bat && alias cat bat
-type -q fd && alias find fd
-type -q lsd && alias ls lsd
-type -q rg && alias grep rg
-type -q zoxide && zoxide init --cmd cd fish | source
-
+#
 # Zellij integration
+#
+
 if set -q ZELLIJ
     function zellij_update_name --on-variable PWD
         if test $PWD = $HOME
@@ -44,3 +18,55 @@ if set -q ZELLIJ
 
     zellij_update_name
 end
+
+#
+# Environment
+#
+
+set -gx EDITOR /bin/helix
+set -gx SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/ssh-agent.socket"
+
+#
+# Bindings
+#
+
+bind \ce $EDITOR
+
+#
+# Program abbreviations
+#
+
+# Change directory
+abbr -a cd- cd -
+abbr -a cd.. cd ..
+
+# Cargo
+abbr -a cb cargo build
+abbr -a cbr cargo build --release
+abbr -a c cargo
+abbr -a cr cargo run
+abbr -a crr cargo run --release
+
+# Dotfiles
+abbr -a dfsa dotfiles add .
+abbr -a dfs dotfiles
+abbr -a dfss dotfiles status
+abbr -a --set-cursor dfsc dotfiles commit -m '%'
+
+# Git
+abbr -a ga git add .
+abbr -a g git
+abbr -a gs git status
+abbr -a --set-cursor gc git commit -m '%'
+
+# Editors
+abbr -a hx helix
+abbr -a vim nvim
+abbr -a vi nvim
+
+# Program replacements
+type -q bat && alias cat bat
+type -q fd && alias find fd
+type -q lsd && alias ls lsd
+type -q rg && alias grep rg
+type -q zoxide && zoxide init --cmd cd fish | source
