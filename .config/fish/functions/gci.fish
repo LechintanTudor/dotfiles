abc23e852cdd56395e14d53cbd3596374790098a
Tudor Lechințan <lechintantudor02@gmail.com>
2024-10-20 19:26:14 +0300

    Add "inside repository" check for gci

--- .config/fish/functions/gci.fish
+++ .config/fish/functions/gci.fish
@@ -1,4 +1,9 @@
 function gci
+    if not git rev-parse --is-inside-work-tree >/dev/null 2>&1
+        echo "Not inside a Git repository."
+        return
+    end
+
     git branch --format '%(refname:short)' |
         fzf \
             # Search
