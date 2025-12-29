1e874d0167b35cc1dbc4df044214d28b49e43237
Tudor Lechințan <lechintantudor02@gmail.com>
2025-12-29 16:38:29 +0200

    Update fzf theme

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -91,6 +91,7 @@ set -gx FZF_DEFAULT_OPTS '
     --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc
     --color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8
     --color=selected-bg:#45475a
+    --color=border:#6c7086,label:#cdd6f4
 '
 
 # Helix
@@ -120,30 +121,30 @@ end
 # Theme
 #
 
-set --global fish_color_autosuggestion 555 brblack
-set --global fish_color_cancel -r
-set --global fish_color_command blue
-set --global fish_color_comment red
-set --global fish_color_cwd green
-set --global fish_color_cwd_root red
-set --global fish_color_end green
-set --global fish_color_error brred
-set --global fish_color_escape brcyan
-set --global fish_color_history_current --bold
-set --global fish_color_host normal
-set --global fish_color_host_remote yellow
-set --global fish_color_normal normal
-set --global fish_color_operator brcyan
-set --global fish_color_param cyan
-set --global fish_color_quote yellow
-set --global fish_color_redirection cyan --bold
-set --global fish_color_search_match bryellow --background=brblack
-set --global fish_color_selection white --bold --background=brblack
-set --global fish_color_status red
-set --global fish_color_user brgreen
-set --global fish_color_valid_path --underline
-set --global fish_pager_color_completion normal
-set --global fish_pager_color_description B3A06D yellow -i
-set --global fish_pager_color_prefix normal --bold --underline
-set --global fish_pager_color_progress brwhite --background=cyan
-set --global fish_pager_color_selected_background -r
+set -g fish_color_autosuggestion 555 brblack
+set -g fish_color_cancel -r
+set -g fish_color_command blue
+set -g fish_color_comment red
+set -g fish_color_cwd green
+set -g fish_color_cwd_root red
+set -g fish_color_end green
+set -g fish_color_error brred
+set -g fish_color_escape brcyan
+set -g fish_color_history_current --bold
+set -g fish_color_host normal
+set -g fish_color_host_remote yellow
+set -g fish_color_normal normal
+set -g fish_color_operator brcyan
+set -g fish_color_param cyan
+set -g fish_color_quote yellow
+set -g fish_color_redirection cyan --bold
+set -g fish_color_search_match bryellow --background=brblack
+set -g fish_color_selection white --bold --background=brblack
+set -g fish_color_status red
+set -g fish_color_user brgreen
+set -g fish_color_valid_path --underline
+set -g fish_pager_color_completion normal
+set -g fish_pager_color_description B3A06D yellow -i
+set -g fish_pager_color_prefix normal --bold --underline
+set -g fish_pager_color_progress brwhite --background=cyan
+set -g fish_pager_color_selected_background -r
